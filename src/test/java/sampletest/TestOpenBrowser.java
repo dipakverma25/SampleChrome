b2c17e37fc0e26d7d8b77145d3c38d3361386de6
Deepakv <deepakv>
2020-04-17 13:59:50 +0530

    aa

--- src/test/java/sampletest/TestOpenBrowser.java
+++ src/test/java/sampletest/TestOpenBrowser.java
@@ -9,13 +9,7 @@ public class TestOpenBrowser {
     private WebDriver webDriver;
 
     public TestOpenBrowser() {
-//        try {
-//            System.setProperty("webdriver.chrome.driver", System.getenv("path"));
-//        }catch (Exception e) {
-//            System.out.println("hhh" + e.getMessage());
-//        }
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEWEBDRIVER")+"\\chromedriver.exe");
-//        System.out.println( System.getenv("CHROMEWEBDRIVER"));
         webDriver = new ChromeDriver();
     }
 
