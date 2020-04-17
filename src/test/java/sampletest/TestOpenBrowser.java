3920bc38fa4983706769f91a6b1f3f5c1b02bc61
Deepakv <deepakv>
2020-04-17 15:49:04 +0530

    aa

--- src/test/java/sampletest/TestOpenBrowser.java
+++ src/test/java/sampletest/TestOpenBrowser.java
@@ -2,6 +2,7 @@ package sampletest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.Test;
 
 public class TestOpenBrowser {
@@ -9,6 +10,7 @@ public class TestOpenBrowser {
     private WebDriver webDriver;
 
     public TestOpenBrowser() {
+
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEWEBDRIVER")+"\\chromedriver.exe");
         webDriver = new ChromeDriver();
     }
